f0ff96d910b540d2ab0a0f9b7faef0fb5ced85cc
Caio Roberto <caiorrs@gmail.com>
2022-02-04 07:31:33 -0300

    JS in JSX with variables and map

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 // import React from "react";
 
 function App() {
-  // Without syntactic sugar
-  // return React.createElement("div", { className: "container" }, React.createElement("h1", {}, "My App"));
+  const title = "Blog post";
+  const body = "This is my blog post";
+  const comments = [
+    {
+      id: 1,
+      text: "Comment one",
+    },
+    {
+      id: 2,
+      text: "Comment two",
+    },
+    {
+      id: 3,
+      text: "Comment three",
+    },
+  ];
 
   return (
     <div className="container">
-      <h1>My App</h1>
-      <p>Hello</p>
+      <h1>{title}</h1>
+      <p>{body}</p>
+      <div className="comments">
+        <h3>Comments ({comments.length})</h3>
+        <ul>
+          {comments.map((comment) => (
+            <li key={comment.id}>{comment.text}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
