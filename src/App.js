eb4b0edb0861ad763451b62b7c312b089fb469f1
Caio Roberto <caiorrs@gmail.com>
2022-02-04 07:25:33 -0300

    JSX intro

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+// import React from "react";
+
 function App() {
-  return <h1>Hello from the App component</h1>;
+  // Without syntactic sugar
+  // return React.createElement("div", { className: "container" }, React.createElement("h1", {}, "My App"));
+
+  return (
+    <div className="container">
+      <h1>My App</h1>
+      <p>Hello</p>
+    </div>
+  );
 }
 
 export default App;
