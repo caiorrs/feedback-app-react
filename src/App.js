aca97838f2abcc4c984af9bad6837e0303c288c3
Caio Roberto <caiorrs@gmail.com>
2022-02-05 08:32:34 -0300

    Created a Feedback context

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Card from "./components/shared/Card";
 import FeedbackData from "./data/FeedbackData";
 import FeedbackForm from "./components/FeedbackForm";
 import FeedbackList from "./components/FeedbackList";
+import { FeedbackProvider } from "./context/FeedbackContext";
 import FeedbackStats from "./components/FeedbackStats";
 import Header from "./components/Header";
 import Post from "./components/Post";
@@ -27,35 +28,37 @@ function App() {
   };
 
   return (
-    <Router>
-      <Header />
-      <div className="container">
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <FeedbackForm handleAdd={addFeedback} />
-                <FeedbackStats feedbacks={feedbacks} />
-                <FeedbackList feedbacks={feedbacks} handleDelete={deleteFeedback} />
-              </>
-            }
-          />
-          <Route path="/about" element={<AboutPage />} />
-          {/* <Route path="/post/:id" element={<Post />} /> */}
-          <Route path="/post/*" element={<Post />} />
-        </Routes>
-        <Card>
-          <NavLink to="/" className={({ isActive }) => (isActive ? "active" : "")}>
-            Home
-          </NavLink>
-          <NavLink to="/about" className={({ isActive }) => (isActive ? "active" : "")}>
-            About
-          </NavLink>
-        </Card>
-        <AboutIconLink />
-      </div>
-    </Router>
+    <FeedbackProvider>
+      <Router>
+        <Header />
+        <div className="container">
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <>
+                  <FeedbackForm handleAdd={addFeedback} />
+                  <FeedbackStats feedbacks={feedbacks} />
+                  <FeedbackList feedbacks={feedbacks} handleDelete={deleteFeedback} />
+                </>
+              }
+            />
+            <Route path="/about" element={<AboutPage />} />
+            {/* <Route path="/post/:id" element={<Post />} /> */}
+            <Route path="/post/*" element={<Post />} />
+          </Routes>
+          <Card>
+            <NavLink to="/" className={({ isActive }) => (isActive ? "active" : "")}>
+              Home
+            </NavLink>
+            <NavLink to="/about" className={({ isActive }) => (isActive ? "active" : "")}>
+              About
+            </NavLink>
+          </Card>
+          <AboutIconLink />
+        </div>
+      </Router>
+    </FeedbackProvider>
   );
 }
 
---
+++ src/context/FeedbackContext.js
@@ -0,0 +1,17 @@
+import { createContext, useState } from "react";
+
+const FeedbackContext = createContext();
+
+export const FeedbackProvider = ({ children }) => {
+  const [feedbacks, setFeedbacks] = useState([
+    {
+      id: 1,
+      text: "This item is from context",
+      rating: 10,
+    },
+  ]);
+
+  return <FeedbackContext.Provider value={{ feedbacks }}>{children}</FeedbackContext.Provider>;
+};
+
+export default FeedbackContext;
